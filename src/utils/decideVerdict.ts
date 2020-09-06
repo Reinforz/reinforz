680933a362c7634630870f4ed5cdd71652845906
Devon Reid <fireborn1003@gmail.com>
2020-09-06 19:38:19 +0600

    FEAT: Added basic option modifiers

--- src/utils/decideVerdict.ts
+++ src/utils/decideVerdict.ts
@@ -1,19 +1,32 @@
 import { QuestionType } from "../types";
 
+const expandOptions = (option: string): string[] => {
+  const options: string[] = [];
+  option.split(",").forEach(option => {
+    const modifiers = option.match(/^_(\w{2}\s?)*_/);
+    if (modifiers) {
+      option = option.replace(modifiers[0], '');
+      const mods = modifiers[0].replace(/_/g, '').split(" ");
+      if (mods.includes("IC")) options.push(option.charAt(0).toUpperCase() + option.substr(1), option.toUpperCase());
+      if (mods.includes("IS")) options.push(option.replace(/\s/g, ''));
+    }
+    options.push(option);
+  })
+  return options;
+}
+
 export default function (type: QuestionType, answers: string[], user_answers: string[]) {
   let verdict: boolean = false;
   switch (type) {
     case "MCQ":
       verdict = answers.length === user_answers.length && answers[0].toString() === user_answers[0].toString();
       break;
-    case "Snippet":
-      verdict = answers.length === user_answers.length && answers[0].split(",").includes(user_answers[0].toString());
-      break;
     case "MS":
       verdict = user_answers.length === answers.length && user_answers.every((user_answer) => answers.includes(user_answer));
       break;
+    case "Snippet":
     case "FIB":
-      verdict = user_answers.length === answers.length && user_answers.every((user_answer, i) => answers[i].split(",").includes(user_answer));
+      verdict = user_answers.length === answers.length && user_answers.every((user_answer, i) => expandOptions(answers[i]).includes(user_answer));
       break;
   }
   return verdict;
