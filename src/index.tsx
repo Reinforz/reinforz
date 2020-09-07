3919f8c0fda9beb333e909da216d54223904b098
Devon Reid <fireborn1003@gmail.com>
2020-09-07 09:46:48 +0600

    STYLE: Added navbar styling
    Basic navbar styling using styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,51 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import styled from "styled-components";
 import { ThemeProvider } from '@material-ui/styles';
 import { Switch, Route, BrowserRouter as Router, withRouter, RouteChildrenProps, NavLink } from "react-router-dom";
 
 import './index.css';
 import './App.css';
-
 import THEME from "./utils/theme";
 import Quiz from "./components/Quiz"
 import Upload from "./components/Upload"
+
+const NavBar = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: space-evenly;
+  font-size: 1.25rem;
+`;
+
+const NavBarLink = styled(NavLink)`
+  text-decoration: none;
+  color: white;
+  padding: 5px;
+  background: #383838;
+  width: 50%;
+  text-align: center;
+  margin: 5px;
+  border-radius: 5px;
+`;
+
 
 const App = (props: RouteChildrenProps) => {
   let [QuizData, setQuizData] = useState({} as any);
   const { location } = props;
   return <div className="App">
-    <NavLink
-      to="/upload"
-      activeStyle={{
-        fontWeight: "bold",
-        color: "red"
-      }}
-    >
-      Upload
-    </NavLink>
-    <NavLink
-      to="/quiz"
-      activeStyle={{
-        fontWeight: "bold",
-        color: "red"
-      }}
-    >
-      Quiz
-    </NavLink>
+    <NavBar>
+      <NavBarLink
+        to="/upload"
+      >
+        Upload
+      </NavBarLink>
+      <NavBarLink
+        to="/quiz"
+      >
+        Quiz
+      </NavBarLink>
+
+    </NavBar>
     <Switch location={location}>
       <Route exact path="/" render={() => <div>Home</div>} />
       <Route path="/upload" render={() => <Upload currentQuiz={QuizData} setQuiz={setQuizData} />} />
