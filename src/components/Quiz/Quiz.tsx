37ecaa1e38648fb5f96423a1ec7fa4144d16d54c
Devon Reid <fireborn1003@gmail.com>
2020-09-21 18:10:11 +0600

    FEAT: Added Regex And Flag Modifiers
    Now each answers can be checked using a regex by using REGEX Modifier along with FLAG modifier to specify flags

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -65,17 +65,25 @@ export default function Quiz(props: QuizProps) {
                 for (let i = 0; i < correct_answers.length; i++) {
                   let mod_user_answer = user_answer;
                   let correct_answer = correct_answers[i];
-                  let modifiers = correct_answer.match(/_(.*?)_(.+)/);
-                  if (modifiers) {
-                    correct_answer = modifiers[2];
-                    modifiers = modifiers[1].split(" ");
+                  const matches = correct_answer.match(/_(.*?)_(.+)/);
+                  if (matches) {
+                    correct_answer = matches[2];
+                    const modifiers = matches[1].split(" ");
+                    const contains_regex_mod = modifiers.includes("REGEX")
                     if (modifiers.includes("IC")) {
-                      correct_answer = correct_answer.toLowerCase();
+                      if (!contains_regex_mod)
+                        correct_answer = correct_answer.toLowerCase();
                       mod_user_answer = mod_user_answer.toLowerCase();
                     }
                     if (modifiers.includes("IS")) {
-                      correct_answer = correct_answer.replace(/\s/g, '');
+                      if (!contains_regex_mod)
+                        correct_answer = correct_answer.replace(/\s/g, '');
                       mod_user_answer = mod_user_answer.replace(/\s/g, '');
+                    }
+                    if (contains_regex_mod) {
+                      const flags = modifiers.find(modifier => modifier.match(/FLAGS=\w+/));
+                      const does_match = Boolean(mod_user_answer.match(new RegExp(String(correct_answer), flags?.split("=")[1] ?? "")));
+                      if (does_match) correct_answer = mod_user_answer;
                     }
                   }
                   isCorrect = correct_answer === mod_user_answer;
