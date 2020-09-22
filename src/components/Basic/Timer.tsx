e612aa2793d59c07357da6097a562d9454a77cab
Devon Reid <fireborn1003@gmail.com>
2020-09-22 18:59:41 +0600

    RF: Refactored Timer State logic to useTimer hook
    Now useTimer can be used on any Component to share the timer state logic

--- src/components/Basic/Timer.tsx
+++ src/components/Basic/Timer.tsx
@@ -1,42 +1,17 @@
 import { useTheme } from "@material-ui/styles";
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 
-import SettingsContext from "../../context/SettingsContext";
+import useTimer from "../../hooks/useTimer";
 
-import { TimerProps, TimerRProps, ExtendedTheme, ISettings } from "../../types";
+import { TimerProps, TimerRProps, ExtendedTheme } from "../../types";
 
 import "./Timer.scss";
 
-const tic = new Audio(process.env.PUBLIC_URL + "/sounds/tic.mp3");
-tic.volume = 0.5;
-
 export default function (props: TimerProps) {
-  const settings = useContext(SettingsContext) as ISettings;
 
-  const [timeout, setTimeout] = useState(props.timeout);
+  const { timeout, displayTime } = useTimer(props.timeout, props.onTimerEnd);
 
   const theme = useTheme() as ExtendedTheme
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (timeout !== 0)
-        setTimeout((seconds) => {
-          if (settings.sound) tic.play();
-          return seconds - 1
-        })
-      else
-        props.onTimerEnd()
-    }, 1000);
-    return () => {
-      clearInterval(timer);
-    }
-  })
-
-  const displayTime = () => {
-    const min = Math.floor(timeout / 60);
-    const sec = timeout % 60;
-    return `0${min}:${sec < 10 ? '0' + sec : sec}`;
-  };
 
   return props.children({
     TimerComponent: <div style={{ backgroundColor: theme.color.dark, color: theme.palette.text.primary }} className="Timer">{displayTime()}</div>,
---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState, useContext } from "react";
+
+import SettingsContext from "../context/SettingsContext";
+
+import { ISettings } from "../types";
+
+const tic = new Audio(process.env.PUBLIC_URL + "/sounds/tic.mp3");
+tic.volume = 0.5;
+
+export default function (limit: number, onTimerEnd: any) {
+  const [timeout, setTimeout] = useState(limit);
+
+  const settings = useContext(SettingsContext) as ISettings;
+
+  const displayTime = () => {
+    const min = Math.floor(timeout / 60);
+    const sec = timeout % 60;
+    return `0${min}:${sec < 10 ? '0' + sec : sec}`;
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (timeout !== 0)
+        setTimeout((seconds) => {
+          if (settings.sound) tic.play();
+          return seconds - 1
+        })
+      else
+        onTimerEnd()
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    }
+  })
+
+  return {
+    timeout,
+    displayTime
+  }
+}
