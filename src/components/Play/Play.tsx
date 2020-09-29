7b8448162ace26f530835493811fbca2d5b69ec4
Devon Reid <fireborn1003@gmail.com>
2020-09-29 14:53:40 +0600

    RF: Refactored the RP CBH problem
    The Play component has been refatored to use functional composition to get rid of callback hell, might change to use react-adopt or react-compose

--- src/components/Play/Play.tsx
+++ src/components/Play/Play.tsx
@@ -38,62 +38,103 @@ const setToLs = debounced((size: string) => {
   localStorage.setItem("Play_pane_size", size || "50%")
 }, 2500)
 
-function Play() {
-  const [playing, setPlaying] = useState(false);
+interface IPlayState {
+  playing: boolean
+}
+
+interface IPlayUtils {
+  setPlaying: (playing: boolean) => void
+}
+
+interface IPlayRProps {
+  PlayState: IPlayState,
+  PlayUtils: IPlayUtils
+}
+
+const renderPlayUpload = (PlayRenderProps: IPlayRProps) =>
+  <PlayUpload>
+    {(PlayUploadRenderProps: PlayUploadRProps) =>
+      renderList({ PlayUploadRenderProps, PlayRenderProps })
+    }
+  </PlayUpload>
+
+const renderList = ({ PlayUploadRenderProps, PlayRenderProps }: { PlayRenderProps: IPlayRProps, PlayUploadRenderProps: PlayUploadRProps }) => {
+  const { PlayUploadState, PlayUploadUtils } = PlayUploadRenderProps;
+  return <List header="Uploaded Quizzes" items={PlayUploadState.items} setItems={PlayUploadUtils.setItems} fields={["subject", "title", (item: any) => item.questions.length + " Qs"]} onDelete={PlayUploadUtils.removeErrorLogs}>
+    {(ListRenderProps: ListRProps) =>
+      renderPlaySettings({ ListRenderProps, PlayUploadRenderProps, PlayRenderProps })
+    }
+  </List>
+}
+
+const renderPlaySettings = ({ ListRenderProps, PlayUploadRenderProps, PlayRenderProps }: { PlayRenderProps: IPlayRProps, ListRenderProps: ListRProps, PlayUploadRenderProps: PlayUploadRProps }) => {
+  const { ListState, ListUtils } = ListRenderProps;
+  const { PlayUploadState, PlayUploadUtils } = PlayUploadRenderProps;
+  const { PlayState: { playing }, PlayUtils: { setPlaying } } = PlayRenderProps;
+  return <PlaySettings quizzes={PlayUploadState.items} setPlaying={setPlaying} selectedQuizzes={ListState.selectedItems}>
+    {(IPlaySettingsRenderProps: IPlaySettingsRProps) => {
+      const { PlaySettingsState, PlaySettingsExtra } = IPlaySettingsRenderProps;
+      if (playing)
+        localStorage.setItem('PLAY_SETTINGS', JSON.stringify(PlaySettingsState))
+      return playing ?
+        <PlayContext.Provider value={{ setQuizzes: PlayUploadUtils.setItems, setPlaying, setSelected: ListUtils.setSelectedItems }}>
+          <Quiz selected_quizzes={PlaySettingsExtra.selected_quizzes} play_options={PlaySettingsState.play_options} all_questions={PlaySettingsExtra.filtered_questions} />
+        </PlayContext.Provider> : renderPlayMenu({ ListRenderProps, PlayUploadRenderProps, IPlaySettingsRenderProps })
+    }}
+  </PlaySettings>
+}
+
+const renderPlayMenu = ({ ListRenderProps, PlayUploadRenderProps, IPlaySettingsRenderProps }: { ListRenderProps: ListRProps, PlayUploadRenderProps: PlayUploadRProps, IPlaySettingsRenderProps: IPlaySettingsRProps }) => {
+  const { PlaySettingsComponent } = IPlaySettingsRenderProps;
+  return <div className="Play" id="Play">
+    <Menu content={PlaySettingsComponent} lskey="Play_menu">
+      {(MenuRenderProps: MenuRProps) =>
+        <Fragment>
+          {MenuRenderProps.MenuComponent}
+          <PlayContent renderprops={{ ListRenderProps, PlayUploadRenderProps, MenuRenderProps }} />
+        </Fragment>
+      }
+    </Menu>
+  </div>
+}
+
+const PlayContent = (props: { renderprops: { ListRenderProps: ListRProps, PlayUploadRenderProps: PlayUploadRProps, MenuRenderProps: MenuRProps } }) => {
+  const { ListRenderProps, PlayUploadRenderProps, MenuRenderProps } = props.renderprops;
   const history = useHistory();
   const { theme, settings } = useThemeSettings();
 
-  return (
-    <PlayUpload>
-      {({ PlayUploadState, PlayUploadComponents, PlayUploadUtils }: PlayUploadRProps) => {
-        return <List header="Uploaded Quizzes" items={PlayUploadState.items} setItems={PlayUploadUtils.setItems} fields={["subject", "title", (item: any) => item.questions.length + " Qs"]} onDelete={PlayUploadUtils.removeErrorLogs}>
-          {({ ListComponent, ListState, ListUtils }: ListRProps) => {
-            return <PlaySettings quizzes={PlayUploadState.items} setPlaying={setPlaying} selectedQuizzes={ListState.selectedItems}>
-              {({ PlaySettingsComponent, PlaySettingsState, PlaySettingsExtra }: IPlaySettingsRProps) => {
-                if (playing)
-                  localStorage.setItem('PLAY_SETTINGS', JSON.stringify(PlaySettingsState))
-                return <Fragment>
-                  {playing ?
-                    <PlayContext.Provider value={{ setQuizzes: PlayUploadUtils.setItems, setPlaying, setSelected: ListUtils.setSelectedItems }}>
-                      <Quiz selected_quizzes={PlaySettingsExtra.selected_quizzes} play_options={PlaySettingsState.play_options} all_questions={PlaySettingsExtra.filtered_questions} />
-                    </PlayContext.Provider> :
-                    <div className="Play" id="Play">
-                      <Menu content={PlaySettingsComponent} lskey="Play_menu">
-                        {({ MenuComponent, MenuExtra }: MenuRProps) => {
-                          return <Fragment>
-                            {MenuComponent}
-                            <div className="Play-content" id="Play-content" style={{ ...MenuExtra.content_elem_style }}>
-                              <Fragment>
-                                <Icon onClick={() => {
-                                  if (settings.sound) swoosh.play()
-                                  history.push("/settings")
-                                }} icon={FcSettings} popoverText="Click to go to settings page" className="App-icon App-icon--settings" />
-                                <SplitPane split="vertical" onChange={(size: any) => {
-                                  setToLs(size[0])
-                                }}>
+  const { ListComponent } = ListRenderProps;
+  const { PlayUploadComponents, PlayUploadState } = PlayUploadRenderProps;
+  const { MenuExtra } = MenuRenderProps;
 
-                                  <Pane initialSize={prev_pane_size || "50%"} minSize="30%" maxSize="70%" className="Play-pane">
-                                    {PlayUploadComponents.PlayUpload}
-                                    <View items={[PlayUploadComponents.PlayErrorLogs, ListComponent]} />
-                                    <div className="Help" style={{ backgroundColor: theme.color.dark, color: theme.palette.text.primary }}>Need help, <a style={{ color: theme.palette.text.secondary }} href="http://github.com/Devorein/reinforz" rel="noopener noreferrer" target="_blank">click here</a> to go to the doc</div>
-                                  </Pane>
-                                  <PlayTable quizzes={PlayUploadState.items} />
-                                </SplitPane>
-                              </Fragment>
-                            </div>
-                          </Fragment>
-                        }}
-                      </Menu>
-                    </div>
-                  }
-                </Fragment>
-              }}
-            </PlaySettings>
-          }}
-        </List>
-      }}
-    </PlayUpload >
-  );
+  return <div className="Play-content" id="Play-content" style={{ ...MenuExtra.content_elem_style }}>
+    <Fragment>
+      <Icon onClick={() => {
+        if (settings.sound) swoosh.play()
+        history.push("/settings")
+      }} icon={FcSettings} popoverText="Click to go to settings page" className="App-icon App-icon--settings" />
+      <SplitPane split="vertical" onChange={(size: any) => {
+        setToLs(size[0])
+      }}>
+        <Pane initialSize={prev_pane_size || "50%"} minSize="30%" maxSize="70%" className="Play-pane">
+          {PlayUploadComponents.PlayUpload}
+          <View items={[PlayUploadComponents.PlayErrorLogs, ListComponent]} />
+          <div className="Help" style={{ backgroundColor: theme.color.dark, color: theme.palette.text.primary }}>Need help, <a style={{ color: theme.palette.text.secondary }} href="http://github.com/Devorein/reinforz" rel="noopener noreferrer" target="_blank">click here</a> to go to the doc</div>
+        </Pane>
+        <PlayTable quizzes={PlayUploadState.items} />
+      </SplitPane>
+
+    </Fragment>
+  </div>
+}
+
+function Play() {
+  const [playing, setPlaying] = useState(false);
+
+  return renderPlayUpload({
+    PlayUtils: { setPlaying },
+    PlayState: { playing }
+  });
 }
 
 export default Play;
--- src/hooks/useCycle.ts
+++ src/hooks/useCycle.ts
@@ -1,6 +1,21 @@
 import { useState } from "react";
 
-export default function (items: any[]) {
+export interface UseCycleOutput {
+  // Current item obtained from current_index
+  current_item: any;
+  // Used to get the next item in the cycle
+  getNextIndex: () => void;
+  // Used to get the prev item in the cycle
+  getPrevIndex: () => void;
+  // Checks whether or not the cycle has been xhausted
+  hasEnded: boolean;
+  // Get the current index of the cycle
+  current_index: number;
+  // Checks whether current_index is the last item
+  is_last_item: boolean;
+}
+
+export default function (items: any[]): UseCycleOutput {
   const [current_index, setCurrentIndex] = useState(0);
 
   return {
@@ -16,3 +31,4 @@ export default function (items: any[]) {
     is_last_item: current_index === items.length - 1
   }
 }
+
