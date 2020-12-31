5d3c41ead01c177b21581b73b540634012b64775
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-31 18:47:39 +0600

    RF: Refactored useList hook to use redux style

--- src/hooks/useList.ts
+++ src/hooks/useList.ts
@@ -1,35 +1,87 @@
-import { useEffect, useState } from "react";
-import { usePrevious } from "react-use";
+import { useReducer, useCallback } from "react";
 
-export default function (items: any[], setItems: any) {
-  const [selectedItems, setSelectedItems] = useState([] as string[])
+interface State<T extends { id: string }> {
+  items: T[],
+  selected_items: string[]
+}
 
-  const prevItems = usePrevious(items);
-  const prev_items_ids = prevItems?.map(prevItem => prevItem._id) ?? []
-  const new_items = items.filter(item => !prev_items_ids.includes(item._id)).map(item => item._id)
+type Action = {
+  type: "SELECT_ALL",
+} | {
+  type: "DESELECT_ALL"
+} | {
+  type: "SELECT",
+  payload: { ids: string[] }
+} | {
+  type: "DESELECT",
+  payload: { ids: string[] }
+} | {
+  type: "DELETE_SELECTED"
+} | {
+  type: "DELETE",
+  payload: { id: string }
+}
 
-  useEffect(() => {
-    setSelectedItems([...selectedItems, ...new_items])
-    // eslint-disable-next-line
-  }, [items])
+function useListReducer<T extends { id: string }>(state: State<T>, action: Action): State<T> {
+  switch (action.type) {
+    case "DELETE_SELECTED": {
+      const items = state.items.filter(item => !state.selected_items.includes(item.id))
+      return {
+        items,
+        selected_items: []
+      }
+    }
+    case "DELETE":
+      const items = state.items.filter(item => action.payload.id !== item.id),
+        selected_items = state.selected_items.filter(id => action.payload.id !== id);
+      return {
+        items,
+        selected_items
+      }
+    case "DESELECT":
+      return {
+        items: state.items,
+        selected_items: state.selected_items.filter(id => !action.payload.ids.includes(id))
+      }
+    case "DESELECT_ALL":
+      return {
+        items: state.items,
+        selected_items: []
+      }
+    case "SELECT":
+      state.selected_items.push(...action.payload.ids)
+      return state;
+    case "SELECT_ALL":
+      return {
+        items: state.items,
+        selected_items: state.items.map(item => item.id)
+      }
+  }
+}
 
-  const resetSelectedItems = () => setSelectedItems([]);
-  const setAllSelected = () => setSelectedItems(items.map(item => item._id));
-  const deleteSelectedItems = () => {
-    const new_items = items.filter(item => !selectedItems.includes(item._id))
-    setItems(new_items);
-    setSelectedItems([])
-    return new_items;
-  };
+export default function <T extends { id: string }>(initial_items: T[]) {
+  const [{ items, selected_items }, dispatch] = useReducer(useListReducer, {
+    items: initial_items,
+    selected_items: []
+  })
 
-  const total_selected = selectedItems.length;
+  const deleteSelectedItems = useCallback(() => dispatch({ type: "DELETE_SELECTED" }), []),
+    deleteItem = useCallback((id: string) => dispatch({ type: "DELETE", payload: { id } }), []),
+    deselectItems = useCallback((ids: string[]) => dispatch({ type: "DESELECT", payload: { ids } }), []),
+    selectItems = useCallback((ids: string[]) => dispatch({ type: "SELECT", payload: { ids } }), []),
+    deselectAllItems = useCallback(() => dispatch({ type: "DESELECT_ALL" }), []),
+    selectAllItems = useCallback(() => dispatch({ type: "SELECT_ALL" }), [])
 
   return {
-    resetSelectedItems,
-    setAllSelected,
+    items,
+    selected_items,
+    total_selected: selected_items.length,
+    total_items: items.length,
     deleteSelectedItems,
-    total_selected,
-    selectedItems,
-    setSelectedItems,
+    deleteItem,
+    deselectItems,
+    deselectAllItems,
+    selectItems,
+    selectAllItems
   }
 }
