45f340004fb1b73dd60181a44713c46974abb1d9
Safwan Shaheer <devorein00@gmail.com>
2021-05-05 21:24:17 +0600

    Created internal PlayUpload utils module

--- src/components/Play/Upload/index.tsx
+++ src/components/Play/Upload/index.tsx
@@ -1,25 +1,15 @@
 import yaml from 'js-yaml';
 import React, { useState } from 'react';
-import shortid from 'shortid';
 import { PlayErrorlogs, PlayUploadContext } from '..';
 import { Upload } from "../../../components/Basic/Upload";
 import { IQuizInputFull } from '../../../types';
-import { generateQuestionInputConfigs } from '../../../utils';
-import { trimLower } from '../../../utils/trimLower';
 import "./style.scss";
+import { isDuplicate, onResolved } from "./utils";
 
 export function PlayUpload() {
   const [items, setItems] = useState<IQuizInputFull[]>([])
-
   return <PlayUploadContext.Provider value={{ items, setItems }}>
-    <Upload items={items} setItems={setItems} loadData={(ext, result) => ext.match(/(yaml|yml)/) ? yaml.safeLoad(result.toString()) as any : JSON.parse(result.toString())} isDuplicate={(items: IQuizInputFull[], loadedData) => items.findIndex((item) => trimLower(item.title) === trimLower(loadedData.title) && trimLower(item.subject) === trimLower(loadedData.subject)) !== -1} onResolved={(currentItems, newItems) => {
-      return currentItems.concat(newItems.map(quiz => {
-        quiz.questions = quiz.questions.map((question) => generateQuestionInputConfigs(question));
-        if (!quiz._id)
-          quiz._id = shortid()
-        return quiz as IQuizInputFull;
-      }))
-    }} />
+    <Upload items={items} setItems={setItems} loadData={(ext, result) => ext.match(/(yaml|yml)/) ? yaml.safeLoad(result.toString()) as any : JSON.parse(result.toString())} isDuplicate={isDuplicate} onResolved={onResolved} />
     <PlayErrorlogs />
   </PlayUploadContext.Provider>
 }
---
+++ src/components/Play/Upload/utils.ts
@@ -0,0 +1,28 @@
+import shortid from 'shortid';
+import { IQuizInputFull, IQuizInputPartial } from '../../../types';
+import { generateQuestionInputConfigs } from '../../../utils';
+import { trimLower } from '../../../utils/trimLower';
+
+export const isDuplicate = (
+  items: IQuizInputFull[],
+  loadedData: IQuizInputPartial
+) =>
+  items.findIndex(
+    (item) =>
+      trimLower(item.title) === trimLower(loadedData.title) &&
+      trimLower(item.subject) === trimLower(loadedData.subject)
+  ) !== -1;
+export const onResolved = (
+  currentItems: IQuizInputFull[],
+  newItems: IQuizInputPartial[]
+) => {
+  return currentItems.concat(
+    newItems.map((quiz) => {
+      quiz.questions = quiz.questions.map((question) =>
+        generateQuestionInputConfigs(question)
+      );
+      if (!quiz._id) quiz._id = shortid();
+      return quiz as IQuizInputFull;
+    })
+  );
+};
