aadad530beb6eb8dc1d2d9afb2c3894fc3dd4e69
Safwan Shaheer <devorein00@gmail.com>
2021-05-20 20:24:47 +0600

    Removed flattenObj module

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,23 +1,20 @@
-import arrayShuffler from "./arrayShuffler"
-import checkTextAnswer from "./checkTextAnswer"
-import displayTime from "./displayTime"
-import download from "./download"
-import filterUploadedQuizzes from "./filterUploadedQuizzes"
-import flattenObj from "./flattenObj"
-import generateConfigs from "./generateConfigs"
-import generateTheme from "./generateTheme"
-import getAnswerResult from "./getAnswerResult"
-export * from "./createDefaultState"
-export * from "./getSettings"
+import arrayShuffler from './arrayShuffler';
+import checkTextAnswer from './checkTextAnswer';
+import displayTime from './displayTime';
+import download from './download';
+import filterUploadedQuizzes from './filterUploadedQuizzes';
+import generateConfigs from './generateConfigs';
+import generateTheme from './generateTheme';
+import getAnswerResult from './getAnswerResult';
+export * from './createDefaultState';
+export * from './getSettings';
 export {
   displayTime,
   getAnswerResult,
   filterUploadedQuizzes,
   generateTheme,
   generateConfigs,
-  flattenObj,
   download,
   checkTextAnswer,
   arrayShuffler
-}
-
+};
