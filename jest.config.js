2a0afc7e994e58e2c9542fd0723b0685f5f3945b
Safwan Shaheer <devorein00@gmail.com>
2021-05-20 20:42:24 +0600

    Ignored paths from tests

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = async () => {
     testTimeout: 30000,
     testEnvironment: 'jsdom',
     verbose: true,
-    testPathIgnorePatterns: [ '<rootDir>/node_modules', '<rootDir>/dist' ],
+    testPathIgnorePatterns: [ '<rootDir>/node_modules', '<rootDir>/dist', '<rootDir>/src/utils/generateTheme.ts', '<rootDir>/src/utils/arrayShuffler.ts' ],
     modulePathIgnorePatterns: [ '<rootDir>/dist' ],
     roots: [ '<rootDir>/tests' ],
     testMatch: [ '<rootDir>/tests/**/*.test.ts' ],
@@ -13,6 +13,9 @@ module.exports = async () => {
     },
     collectCoverage: true,
     coverageDirectory: "./coverage",
+    collectCoverageFrom: [
+      "src/**/{!(generateTheme|arrayShuffler),}.ts"
+    ]
     /* coverageThreshold: {
       "global": {
         "branches": 95,
