0f58cfea613612f6ffc0cc6eab0ec985b2136ea1
Safwan Shaheer <devorein00@gmail.com>
2021-05-25 16:59:31 +0600

    Added tests for checkInputAnswer function

--- tests/utils/checkInputAnswers.test.ts
+++ tests/utils/checkInputAnswers.test.ts
@@ -1,4 +1,4 @@
-import { modifyAnswers } from '../../src/utils';
+import { checkInputAnswer, modifyAnswers } from '../../src/utils';
 
 describe('modifyAnswers', () => {
   it(`Should modify all answers`, () => {
@@ -11,5 +11,71 @@ describe('modifyAnswers', () => {
     );
     expect(modifiedUserAnswer).toStrictEqual('helloworld');
     expect(modifiedAnswerText).toStrictEqual('helloworld');
+  });
+});
+
+describe('checkInputAnswer', () => {
+  describe('Answer text', () => {
+    it(`Should match regular answer text`, () => {
+      const isCorrect = checkInputAnswer('Hello World', [
+        {
+          modifiers: [],
+          regex: null,
+          text: 'helloworld'
+        },
+        {
+          modifiers: ['IC', 'IS'],
+          regex: null,
+          text: 'Hello World'
+        }
+      ]);
+      expect(isCorrect).toStrictEqual(true);
+    });
+
+    it(`Should not match regular answer text`, () => {
+      const isCorrect = checkInputAnswer('Hello World', [
+        {
+          modifiers: [],
+          regex: null,
+          text: 'helloworld'
+        },
+        {
+          modifiers: [],
+          regex: null,
+          text: 'hello world'
+        }
+      ]);
+      expect(isCorrect).toStrictEqual(false);
+    });
+  });
+
+  describe('Answer regex', () => {
+    it(`Should match regular answer regex`, () => {
+      const isCorrect = checkInputAnswer('Hello World', [
+        {
+          modifiers: [],
+          regex: {
+            regex: 'Hello World',
+            flags: ''
+          },
+          text: 'helloworld'
+        }
+      ]);
+      expect(isCorrect).toStrictEqual(true);
+    });
+
+    it(`Should not match regular answer regex`, () => {
+      const isCorrect = checkInputAnswer('Hello World', [
+        {
+          modifiers: [],
+          regex: {
+            regex: 'HelloWorld',
+            flags: ''
+          },
+          text: 'helloworld'
+        }
+      ]);
+      expect(isCorrect).toStrictEqual(false);
+    });
   });
 });
